fcbe9d722c6249cad59b08292b4aa477930f3c75
acosneanu <artur.cosneanu95@gmail.com>
2024-12-27 10:28:43 +0200

    Added 2 new boolean variable as a practical exercise

---
+++ src/main/java/com/endava/variablesandcomments/CommentsAndVariables.java
@@ -0,0 +1,14 @@
+package com.endava.variablesandcomments;
+
+public class CommentsAndVariables {
+    public static void main(String[] args) {
+        boolean isSnowingOutside;
+        isSnowingOutside = true;
+        System.out.println("The value of our variable is:"+isSnowingOutside);
+        boolean isFriday = true;
+        System.out.println("The value of our variable is:"+isFriday);
+
+    }
+
+
+}
