a46647278ce84a4d45dd799af38c35277840081f
acosneanu <artur.cosneanu95@gmail.com>
2024-12-27 11:13:13 +0200

    Declaration and initialization of the variables plus comments

--- src/main/java/com/endava/variablesandcomments/CommentsAndVariables.java
+++ src/main/java/com/endava/variablesandcomments/CommentsAndVariables.java
@@ -2,11 +2,42 @@ package com.endava.variablesandcomments;
 
 public class CommentsAndVariables {
     public static void main(String[] args) {
-        boolean isSnowingOutside;
-        isSnowingOutside = true;
-        System.out.println("The value of our variable is:"+isSnowingOutside);
-        boolean isFriday = true;
-        System.out.println("The value of our variable is:"+isFriday);
+// Declare the primitive data type variables
+        boolean falseBooleanVariable;
+        boolean trueBooleanVariable;
+        char firstCharFromAscii;
+        char lastCharFromAscii;
+        byte lowerLimitOfByteDataType;
+        byte upperLimitOfByteDataType;
+        short lowerLimitOfShortDataType;
+        short upperLimitOfShortDataType;
+        int lowerLimitOfIntDataType;
+        int upperLimitOfIntDataType;
+        long lowerLimitOfLongDataType;
+        long upperLimitOfLongDataType;
+        float lowerLimitOfFloatDataType;
+        float upperLimitOfFloatDataType;
+        double  lowerLimitOfDoubleDataType;
+        double upperLimitOfDoubleDataType;
+
+//  Initialization of the above variables
+        falseBooleanVariable = false;
+        trueBooleanVariable = true;
+        firstCharFromAscii = '\u0000';  // This is the value for 0 in Unicode (null character)
+        lastCharFromAscii =  '\uFFFF';  // This is the value for 65,535 in Unicode
+        lowerLimitOfByteDataType = -128;
+        upperLimitOfByteDataType = 127;
+        lowerLimitOfShortDataType = -32768;
+        upperLimitOfShortDataType = 32767;
+        lowerLimitOfIntDataType = -2147483648;
+        upperLimitOfIntDataType = 2147483647;
+        lowerLimitOfLongDataType =  -9223372036854775808L;
+        upperLimitOfLongDataType = 9223372036854775807L;
+        lowerLimitOfFloatDataType = -3.4E+38f;
+        upperLimitOfFloatDataType = 3.4E+38f;
+        lowerLimitOfDoubleDataType = -1.7E+308d;
+        upperLimitOfDoubleDataType = 1.7E+308d;
+        System.out.println("The value" +(int) lastCharFromAscii);
 
     }
 
